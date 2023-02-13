2585a57cf08bbecde46a63855065297527e6c044
malinjr07 <malinjr07@gmail.com>
2023-02-13 19:33:01 +0600

    1 file modified

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,24 +1,12 @@
 import FileUploader from '@/components/FileUploader';
-import { SyntheticEvent, useState } from 'react';
+import { useState } from 'react';
 
 export default function Home() {
   const [images, setImages] = useState<any[]>([]);
-  console.log('🚀 ~ file: index.tsx:6 ~ Home ~ images', images);
-  const handleSubmit = (e: SyntheticEvent) => {
-    e.preventDefault();
-    setImages([]);
-  };
-  return (
-    <section className="w-screen h-screen flex justify-center items-center ">
-      <form
-        className="px-6 py-4 rounded-2xl border "
-        method="post"
-        onSubmit={handleSubmit}
-      >
-        <FileUploader updateFilesCb={(e) => setImages(e)} stateData={images} />
 
-        <button>submit</button>
-      </form>
+  return (
+    <section className="w-screen h-screen py flex justify-center items-start ">
+      <FileUploader updateFilesCb={(e) => setImages(e)} stateData={images} />
     </section>
   );
 }
